ef5a8459b352146d425a40dac7e06349a9f0287b
rafaelrodrigopa <rafael.rodrigo.p.a@gmail.com>
2026-07-24 10:22:18 -0300

    refactor(config): remove redundant GCP_PROJECT_ID assignment

--- src/core/config.py
+++ src/core/config.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 class Settings:
     PROJECT_ID = os.getenv("GCP_PROJECT_ID")
-    GCP_PROJECT_ID = PROJECT_ID
     LANDING = os.getenv("BQ_DATASET_LANDING")
     BRONZE = os.getenv("BQ_DATASET_BRONZE")
     SILVER = os.getenv("BQ_DATASET_SILVER")
